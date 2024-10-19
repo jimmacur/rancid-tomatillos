bb8a17d0f56372ea730e89e34a35b3512b3bca67
Jim Macur <jim.macur@gmail.com>
2024-10-18 18:20:43 -0600

    feat: added css styling for movie poster container and app

--- src/MoviePoster/MoviePoster.js
+++ src/MoviePoster/MoviePoster.js
@@ -1,12 +1,13 @@
 import './MoviePoster.css';
 import React from 'react';
+import upvote from '../icons/upvote.png';
+import downvote from '../icons/downvote.png';
 
 function MoviePoster( { title, poster, voteCount } ) {
   return (
     <section className='MoviePoster'>
-      <img src={poster} alt={title} /> 
-      <h2>{title}</h2>
-      <p>{voteCount}</p>
+      <img className={'movie-poster'} src={poster} alt={title} /> 
+      <p><span><img className={'upvote-arrow'} src={upvote} /></span>{voteCount}<span><img  className={'downvote-arrow'} src={downvote} /></span></p>
     </section>
   );
 }
